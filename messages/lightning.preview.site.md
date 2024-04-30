67ced757ebe32e7349946f6c0fe031f546061a9b
Nicolas Kruk <nkruk@salesforce.com>
2024-04-30 01:10:56 -0400

    feat: generate stubs

---
+++ messages/lightning.preview.site.md
@@ -0,0 +1,32 @@
+# summary
+
+Preview LWC components in context of Experience Cloud on Experience Delivery site.
+
+# description
+
+Preview components, org, and sites. If no topic is specified, the default action is to preview the org.
+
+In dev preview mode, you can edit local files and see these changes to your Lightning Web Components (LWC) within your {org name} org:
+
+- Basic HTML and CSS edits
+- Importing new CSS-only LWC
+- JS edits in-service component library
+- JS method changes in the LWC component that don't alter its public API.
+
+Other local changes require deployment to your org. However, changes made directly in your org (like modifying component properties and saving) are immediately live and won't show in your local files until you retrieve them from the org.
+
+This feature enables developers to quickly iterate on their components and pages, seeing the impact of changes in real-time without needing to deploy or refresh manually. Live reload is enabled by default to automatically refresh the preview when source code changes are detected.
+
+Use the appropriate topic to preview specific aspects of the development environment.
+
+# flags.name.summary
+
+Description of a flag.
+
+# flags.name.description
+
+More information about a flag. Don't repeat the summary.
+
+# examples
+
+- <%= config.bin %> <%= command.id %>
---
+++ test/commands/lightning/preview/org.test.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { TestContext } from '@salesforce/core/testSetup';
+import { expect } from 'chai';
+import { stubSfCommandUx } from '@salesforce/sf-plugins-core';
+import LightningPreviewOrg from '../../../../src/commands/lightning/preview/org.js';
+
+describe('lightning preview org', () => {
+  const $$ = new TestContext();
+  let sfCommandStubs: ReturnType<typeof stubSfCommandUx>;
+
+  beforeEach(() => {
+    sfCommandStubs = stubSfCommandUx($$.SANDBOX);
+  });
+
+  afterEach(() => {
+    $$.restore();
+  });
+
+  it('runs hello', async () => {
+    await LightningPreviewOrg.run([]);
+    const output = sfCommandStubs.log
+      .getCalls()
+      .flatMap((c) => c.args)
+      .join('\n');
+    expect(output).to.include('hello world');
+  });
+
+  it('runs hello with --json and no provided name', async () => {
+    const result = await LightningPreviewOrg.run([]);
+    expect(result.path).to.equal('/Users/nkruk/git/plugin-lightning-dev/src/commands/lightning/preview/org.ts');
+  });
+
+  it('runs hello world --name Astro', async () => {
+    await LightningPreviewOrg.run(['--name', 'Astro']);
+    const output = sfCommandStubs.log
+      .getCalls()
+      .flatMap((c) => c.args)
+      .join('\n');
+    expect(output).to.include('hello Astro');
+  });
+});
