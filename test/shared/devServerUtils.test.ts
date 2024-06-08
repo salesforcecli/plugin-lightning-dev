68692eaf313382607fa67b91c014cf3b70836bb1
Takashi Arai <t.arai@salesforce.com>
2024-06-07 18:53:32 -0700

    feat: dev server utils test

---
+++ test/shared/devServerUtils.test.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { TestContext } from '@salesforce/core/testSetup';
+import { expect } from 'chai';
+import { CommonUtils } from '@salesforce/lwc-dev-mobile-core';
+import { DevServerUtils } from '../../src/shared/devServerUtils.js';
+
+describe('devServerUtils', () => {
+  const $$ = new TestContext();
+
+  afterEach(() => {
+    $$.restore();
+  });
+
+  it('getServerConfigFileLocation returns joined path of current working directory and lwr.config.json', async () => {
+    $$.SANDBOX.stub(process, 'cwd').returns('desktop');
+    const configFileLocation = DevServerUtils.getServerConfigFileLocation();
+    expect(configFileLocation).to.be.equal('desktop/lwr.config.json');
+  });
+
+  it('fetchServerConfigFileContent returns expected object', async () => {
+    $$.SANDBOX.stub(DevServerUtils, 'getServerConfigFileLocation').returns('foo');
+    $$.SANDBOX.stub(CommonUtils, 'loadJsonFromFile').withArgs('foo').returns({ foo: 'bar' });
+    const config = DevServerUtils.fetchServerConfigFileContent();
+    expect(config).to.deep.equal({ foo: 'bar' });
+  });
+
+  it('writeServerConfigFileContent calls CommonUtils.createTextFile', async () => {
+    $$.SANDBOX.stub(DevServerUtils, 'getServerConfigFileLocation').returns('foo');
+    const stub = $$.SANDBOX.stub(CommonUtils, 'createTextFile').resolves();
+    expect(stub.calledOnce);
+  });
+});
