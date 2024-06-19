c6ed97be9f3dda63b232e212839c44c152449260
Takashi Arai <t.arai@salesforce.com>
2024-06-19 15:57:49 -0700

    chore: remove validation

--- src/configMeta.ts
+++ src/configMeta.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import type { ConfigValue, ConfigPropertyMeta } from '@salesforce/core';
+import type { ConfigPropertyMeta } from '@salesforce/core';
 
 export enum ConfigVars {
   /**
@@ -21,20 +21,5 @@ export default [
     description: 'The Base64-encoded identity token of the local web server',
     hidden: true,
     encrypted: true,
-    input: {
-      validator: (value: ConfigValue): boolean => {
-        // Ensure that `value` is a Base64-encoded string.
-        if (typeof value !== 'string') {
-          return false;
-        }
-        try {
-          const decoded = Buffer.from(value, 'base64').toString('base64');
-          return decoded === value;
-        } catch (e) {
-          return false;
-        }
-      },
-      failedMessage: `${ConfigVars.LOCAL_WEB_SERVER_IDENTITY_TOKEN} is not a Base64-encoded string!`,
-    },
   },
 ] as ConfigPropertyMeta[];
