d13752ce32a45e0709ee3bc7bd83c620112f4e78
Takashi Arai <t.arai@salesforce.com>
2024-06-19 00:04:20 -0700

    chore: nit

--- test/shared/identityUtils.test.ts
+++ test/shared/identityUtils.test.ts
@@ -5,7 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-
 import { expect } from 'chai';
 import { TestContext } from '@salesforce/core/testSetup';
 import { IdentityUtils } from '../../src/shared/identityUtils.js';
