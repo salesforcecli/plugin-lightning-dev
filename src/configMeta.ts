412019c01a65310c437cd0e28ecf7f6c55a71f5d
Takashi Arai <t.arai@salesforce.com>
2024-06-20 19:11:53 -0700

    feat: create web server identity token (#52)
    * feat: create and add web server token
    
    * feat: refactor. add dev server utils
    
    * feat: dev server utils test
    
    * chore: nit
    
    * feat: test. nit
    
    * feat: 256 bit
    
    * chore: update after code review
    
    * chore: hex
    
    * chore: base64. update todo comment
    
    * chore: use updated CryptoUtils
    
    * chore: use sf config
    
    * chore: nit
    
    * chore: remove unused files
    
    * chore: remove validation
    
    * chore: use default local config. update tests
    
    * chore: update after code review
    
    * Update src/shared/identityUtils.ts
    
    Co-authored-by: Abdulsattar Mohammed <abdulsattarmohammed@salesforce.com>
    
    * chore: update tests
    
    * chore: removing unnecessary eslint disable
    
    ---------
    
    Co-authored-by: Abdulsattar Mohammed <abdulsattarmohammed@salesforce.com>

---
+++ src/configMeta.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import type { ConfigPropertyMeta } from '@salesforce/core';
+
+export const enum ConfigVars {
+  /**
+   * The Base64-encoded identity token of the local web server, used to
+   * validate the web server's identity to the hmr-client.
+   */
+  LOCAL_WEB_SERVER_IDENTITY_TOKEN = 'local-web-server-identity-token',
+}
+
+export default [
+  {
+    key: ConfigVars.LOCAL_WEB_SERVER_IDENTITY_TOKEN,
+    description: 'The Base64-encoded identity token of the local web server',
+    hidden: true,
+    encrypted: true,
+  },
+] as ConfigPropertyMeta[];
