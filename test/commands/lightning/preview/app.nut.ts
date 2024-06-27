95c209279b87fc61a6d59521b2924ebda65e6014
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2024-06-27 11:46:52 -0700

    ci: comment out NUT placeholder tests

--- test/commands/lightning/preview/app.nut.ts
+++ test/commands/lightning/preview/app.nut.ts
@@ -4,6 +4,8 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+// TODO - add proper NUT tests
+/*
 import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
 import { expect } from 'chai';
 
@@ -25,3 +27,4 @@ describe('lightning preview org NUTs', () => {
     expect(output).to.contain(name);
   });
 });
+*/
