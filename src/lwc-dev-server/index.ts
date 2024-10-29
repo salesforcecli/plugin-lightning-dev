b382d6bb5a52ba62a388ea6f1b7a2ccc8b9d15de
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2024-10-29 10:17:36 -0400

    fix: consider lwc dirs nested under package directories and pass namespace

--- src/lwc-dev-server/index.ts
+++ src/lwc-dev-server/index.ts
@@ -5,46 +5,18 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { existsSync, lstatSync, readFileSync } from 'node:fs';
-import path from 'node:path';
 import process from 'node:process';
-import { LWCServer, LogLevel, ServerConfig, startLwcDevServer, Workspace } from '@lwc/lwc-dev-server';
-import { Lifecycle, Logger } from '@salesforce/core';
+import { LWCServer, ServerConfig, startLwcDevServer, Workspace } from '@lwc/lwc-dev-server';
+import { Lifecycle, Logger, SfProject } from '@salesforce/core';
 import { SSLCertificateData } from '@salesforce/lwc-dev-mobile-core';
+import { glob } from 'glob';
 import {
   ConfigUtils,
   LOCAL_DEV_SERVER_DEFAULT_HTTP_PORT,
   LOCAL_DEV_SERVER_DEFAULT_WORKSPACE,
 } from '../shared/configUtils.js';
 
-/**
- * Map sf cli log level to lwc dev server log level
- * https://github.com/salesforcecli/cli/wiki/Code-Your-Plugin#logging-levels
- *
- * @param cliLogLevel
- * @returns number
- */
-function mapLogLevel(cliLogLevel: number): number {
-  switch (cliLogLevel) {
-    case 10:
-      return LogLevel.verbose;
-    case 20:
-      return LogLevel.debug;
-    case 30:
-      return LogLevel.info;
-    case 40:
-      return LogLevel.warn;
-    case 50:
-      return LogLevel.error;
-    case 60:
-      return LogLevel.silent;
-    default:
-      return LogLevel.error;
-  }
-}
-
 async function createLWCServerConfig(
-  logger: Logger,
   rootDir: string,
   token: string,
   clientType: string,
@@ -52,30 +24,13 @@ async function createLWCServerConfig(
   certData?: SSLCertificateData,
   workspace?: Workspace
 ): Promise<ServerConfig> {
-  const sfdxConfig = path.resolve(rootDir, 'sfdx-project.json');
+  const project = await SfProject.resolve();
+  const packageDirs = project.getPackageDirectories();
+  const projectJson = await project.resolveProjectConfig();
+  const { namespace } = projectJson;
 
-  if (!existsSync(sfdxConfig) || !lstatSync(sfdxConfig).isFile()) {
-    throw new Error(`sfdx-project.json not found in ${rootDir}`);
-  }
-
-  const sfdxConfigJson = readFileSync(sfdxConfig, 'utf-8');
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { packageDirectories } = JSON.parse(sfdxConfigJson);
-  const namespacePaths: string[] = [];
-
-  for (const dir of packageDirectories) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    if (dir.path) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
-      const resolvedDir = path.resolve(rootDir, dir.path, 'main', 'default');
-      if (existsSync(resolvedDir) && lstatSync(resolvedDir).isDirectory()) {
-        logger.debug(`Adding ${resolvedDir} to namespace paths`);
-        namespacePaths.push(resolvedDir);
-      } else {
-        logger.warn(`Skipping ${resolvedDir} because it does not exist or is not a directory`);
-      }
-    }
-  }
+  // e.g. lwc folders in force-app/main/default/lwc, package-dir/lwc
+  const namespacePaths = (await Promise.all(packageDirs.map((dir) => glob(`${dir.fullPath}/**/lwc`)))).flat();
 
   const ports = serverPorts ??
     (await ConfigUtils.getLocalDevServerPorts()) ?? {
@@ -91,9 +46,9 @@ async function createLWCServerConfig(
     // use custom workspace if any is provided, or fetch from config file (if any), otherwise use the default workspace
     workspace: workspace ?? (await ConfigUtils.getLocalDevServerWorkspace()) ?? LOCAL_DEV_SERVER_DEFAULT_WORKSPACE,
     identityToken: token,
-    logLevel: mapLogLevel(logger.getLevel()),
     lifecycle: Lifecycle.getInstance(),
     clientType,
+    namespace: typeof namespace === 'string' ? namespace : undefined,
   };
 
   if (certData?.pemCertificate && certData.pemPrivateKey) {
@@ -116,10 +71,10 @@ export async function startLWCServer(
   certData?: SSLCertificateData,
   workspace?: Workspace
 ): Promise<LWCServer> {
-  const config = await createLWCServerConfig(logger, rootDir, token, clientType, serverPorts, certData, workspace);
+  const config = await createLWCServerConfig(rootDir, token, clientType, serverPorts, certData, workspace);
 
   logger.trace(`Starting LWC Dev Server with config: ${JSON.stringify(config)}`);
-  let lwcDevServer: LWCServer | null = await startLwcDevServer(config);
+  let lwcDevServer: LWCServer | null = await startLwcDevServer(config, logger);
 
   const cleanup = (): void => {
     if (lwcDevServer) {
