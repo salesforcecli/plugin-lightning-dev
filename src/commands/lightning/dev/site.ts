98262847f216b9badd6bed2f82ef0007bab7e417
Nicolas Kruk <nkruk@salesforce.com>
2024-11-20 15:24:59 -0500

    fix: typescript error

--- src/commands/lightning/dev/site.ts
+++ src/commands/lightning/dev/site.ts
@@ -81,7 +81,7 @@ export default class LightningDevSite extends SfCommand<void> {
       // Start the dev server
       const port = parseInt(process.env.PORT ?? '3000', 10);
       const startupParams: LocalDevOptions = {
-        sfCli: true,
+        sfCLI: true,
         authToken,
         open: true,
         port,
