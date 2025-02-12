41155bf5f90cec75b961b0eeed605ca471cad11b
Nicolas Kruk <nicolaskruk@gmail.com>
2025-02-12 13:46:33 -0500

    feat: scaffolding for single component preview command @W-17803218 (#336)

---
+++ src/commands/lightning/dev/component.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
+import { Messages } from '@salesforce/core';
+import { cmpDev } from '@lwrjs/api';
+
+Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
+const messages = Messages.loadMessages('@salesforce/plugin-lightning-dev', 'lightning.dev.component');
+
+export type LightningDevComponentResult = {
+  path: string;
+};
+
+export default class LightningDevComponent extends SfCommand<LightningDevComponentResult> {
+  public static readonly summary = messages.getMessage('summary');
+  public static readonly description = messages.getMessage('description');
+  public static readonly examples = messages.getMessages('examples');
+
+  public static readonly flags = {
+    name: Flags.string({
+      summary: messages.getMessage('flags.name.summary'),
+      description: messages.getMessage('flags.name.description'),
+      char: 'n',
+      required: false,
+    }),
+    // TODO should this be required or optional?
+    // We don't technically need this if your components are simple / don't need any data from your org
+    'target-org': Flags.requiredOrg(),
+  };
+
+  public async run(): Promise<LightningDevComponentResult> {
+    const { flags } = await this.parse(LightningDevComponent);
+
+    const name = flags.name ?? 'world';
+    this.log(`preview component: ${name}`);
+
+    // TODO implement me
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await cmpDev({
+      componentName: name,
+    });
+
+    return {
+      path: '',
+    };
+  }
+}
