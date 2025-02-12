81f5465e368da59de28802be240d997502f6bfb5
Nicolas Kruk <nkruk@salesforce.com>
2025-02-12 11:31:36 -0500

    feat: single component preview

---
+++ test/commands/lightning/dev/component.test.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { TestContext } from '@salesforce/core/testSetup';
+// import { expect } from 'chai';
+// import { stubSfCommandUx } from '@salesforce/sf-plugins-core';
+
+describe('lightning single component preview', () => {
+  const $$ = new TestContext();
+  // let sfCommandStubs: ReturnType<typeof stubSfCommandUx>;
+
+  beforeEach(() => {
+    // sfCommandStubs = stubSfCommandUx($$.SANDBOX);
+  });
+
+  afterEach(() => {
+    $$.restore();
+  });
+
+  it('todo add unit tests', async () => {});
+});
