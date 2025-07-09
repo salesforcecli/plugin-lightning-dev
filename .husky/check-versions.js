e40b7fcdf33ecd1e6f89240eb9e44f568afba6e9
Abdulsattar Mohammed <abdulsattarmohammed@salesforce.com>
2025-07-09 13:00:54 -0400

    fix: bump LWC to 13.0.29 @W-18283826 (#448)

--- .husky/check-versions.js
+++ .husky/check-versions.js
@@ -1,23 +1,24 @@
 import fs from 'node:fs';
+import semver from 'semver';
 
 // Read package.json
 const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 // Extract versions
 const devServerDependencyVersion = packageJson.dependencies['@lwc/lwc-dev-server'];
-const devServerTargetVersion = packageJson.apiVersionMetadata?.target?.matchingDevServerVersion;
+const devServerTargetVersionRange = packageJson.apiVersionMetadata?.target?.matchingDevServerVersion;
 
-if (!devServerDependencyVersion || !devServerTargetVersion) {
+if (!devServerDependencyVersion || !devServerTargetVersionRange) {
   console.error('Error: missing @lwc/lwc-dev-server or matchingDevServerVersion');
   process.exit(1); // Fail the check
 }
 
 // Compare versions
-if (devServerDependencyVersion === devServerTargetVersion) {
+if (semver.intersects(devServerTargetVersionRange, devServerDependencyVersion)) {
   process.exit(0); // Pass the check
 } else {
   console.error(
-    `Error: @lwc/lwc-dev-server versions do not match between 'dependencies' and 'apiVersionMetadata' in package.json. Expected ${devServerDependencyVersion} in apiVersionMetadata > target > matchingDevServerVersion. Got ${devServerTargetVersion} instead. When updating the @lwc/lwc-dev-server dependency, you must ensure that it is compatible with the supported API version in this branch, then update apiVersionMetadata > target > matchingDevServerVersion to match, in order to "sign off" on this dependency change.`
+    `Error: @lwc/lwc-dev-server versions do not match between 'dependencies' and 'apiVersionMetadata' in package.json. Expected ${devServerDependencyVersion} in apiVersionMetadata > target > matchingDevServerVersion. Got ${devServerTargetVersionRange} instead. When updating the @lwc/lwc-dev-server dependency, you must ensure that it is compatible with the supported API version in this branch, then update apiVersionMetadata > target > matchingDevServerVersion to match, in order to "sign off" on this dependency change.`
   );
   process.exit(1); // Fail the check
 }
