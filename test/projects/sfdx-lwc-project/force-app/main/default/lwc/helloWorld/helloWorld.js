e03665ff6b0d0b4b2957b75f3597a8a71081c37d
lturanscaia <lturanscaia@salesforce.com>
2025-07-11 19:36:03 -0700

    chore: remove redundant helloWorld component from force-app test project

--- test/projects/sfdx-lwc-project/force-app/main/default/lwc/helloWorld/helloWorld.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class HelloWorld extends LightningElement {
-  greeting = 'Hello, World!';
-
-  handleClick() {
-    this.greeting = this.greeting === 'Hello, World!' ? 'Hello, LWC!' : 'Hello, World!';
-  }
-}
