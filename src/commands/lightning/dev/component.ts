45e1bf969b4311899acb3c3e8dc26243c7ee615d
Nicolas Kruk <nkruk@salesforce.com>
2026-02-17 22:10:42 -0500

    fix: update proxy URI scheme and port for Code Builder

--- src/commands/lightning/dev/component.ts
+++ src/commands/lightning/dev/component.ts
@@ -93,9 +93,9 @@ export default class LightningDevComponent extends SfCommand<ComponentPreviewRes
     // In Code Builder, we cannot go to localhost - we need to use a proxy URI to get to the ldpServer
     if (process.env.SF_CONTAINER_MODE && process.env.CODE_BUILDER_FRAMEWORK_PROXY_URI) {
       logger.debug('In Code Builder Mode - using proxy URI');
-      ldpServerUrl = process.env.CODE_BUILDER_FRAMEWORK_PROXY_URI.replace('https://', 'wss://').replace(
+      ldpServerUrl = process.env.CODE_BUILDER_FRAMEWORK_PROXY_URI.replace('https://', 'ws://').replace(
         '{{port}}',
-        `${serverPorts.httpsPort}`,
+        `${serverPorts.httpPort}`,
       );
     } else {
       // Default behavior
